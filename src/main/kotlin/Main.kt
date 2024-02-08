221d3b6bcaa924134ab9af15b303e964c3be07ef
Jemimah-Nthenya <jemimahogonda@gmail.com>
2024-02-08 07:27:32 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+val fullName = "Jemimah Nthenya"
+    println(fullName)
+    var age :Byte = 20
+    println(age)
+    val studenyNumber = +254729944616
+    println("studentNumber")
+    var weight = 54
+    println(weight)
+    val kenyanCitizen = true
+    println(kenyanCitizen)
+
+
+
+
+
+
+}
